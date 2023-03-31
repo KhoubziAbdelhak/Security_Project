829013e4154677dc2e055fe2e2d096f7442d62fd
KhoubziAbdelhak <107383744+KhoubziAbdelhak@users.noreply.github.com>
2023-03-31 18:12:23 +0100

    Update 3des_test.sh
    fixing the time command

--- Activity1/3des_test.sh
+++ Activity1/3des_test.sh
@@ -12,12 +12,12 @@ for file in ${my_array}; do
   echo -n "$number," >> ../$1.csv
   
   echo "Encrypting $file with ECB... "
-  first_time=$( { time -f "%e" openssl enc -e -pbkdf2 -des-ede3 -in $file -out output -k hunter >/dev/null; } 2>&1 )
+  first_time=$( { /bin/time -f "%e" openssl enc -e -pbkdf2 -des-ede3 -in $file -out output -k hunter >/dev/null; } 2>&1 )
   rm output
   echo -n "$first_time," >> ../$1.csv
   
   echo "Encrypting $file with CBC... "
-  second_time=$( { time -f "%e" openssl enc -e -pbkdf2 -des-ede3-cbc -in $file -out output -k hunter >/dev/null; } 2>&1 )
+  second_time=$( { /bin/time -f "%e" openssl enc -e -pbkdf2 -des-ede3-cbc -in $file -out output -k hunter >/dev/null; } 2>&1 )
   rm output
   echo "$second_time" >> ../$1.csv
 
